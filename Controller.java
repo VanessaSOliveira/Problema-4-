794bb2c4f569f3643deed607781eb8c16c068adf
VanessaSOliveira <vanessasouzaoliveira1@gmail.com>
2016-10-17 22:13:13 -0200

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,5 +1,112 @@
 package Controller;
 
+import Util.Grafo;
+import Util.Lista;
+import java.io.File;
+import java.io.IOException;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
+
+public class Controller {
+    private Object[][] matrizGrafo;
+    private String[] bairros;
+    //private int[] distancias;
+    private Grafo grafo;
+    private double precoQuilometro;
+    private Lista arestasMenoresTempo;//Seria bom pra retornar as arestas de menor caminho, mas ppodem ser os vertices tbm
+    private double distanciaDoMenorTempo;
+    private double menorTempo;//n sei se vai precisar
+
+    
+    public Controller(){
+       grafo = new Grafo();
+       matrizGrafo = new Object[50][50];
+       bairros = new String[50];
+       //distancias = new int[50];
+       precoQuilometro = 1;       
+    }
+    
+    public double getPrecoQuilometro(){
+        return precoQuilometro;
+    }
+    public void setPrecoQuilometro(double valor){
+        precoQuilometro = valor;
+    }
+    //retorna valor da corrida
+    public double valorTotalCorrida(){
+        double total;
+        total = distanciaDoMenorTempo * precoQuilometro;
+        return total;
+    }
+      
+    public void insereNaMatriz(String nomeArquivo) throws IOException{
+        
+        try {
+            //Pega planilha com todas as abas
+            Workbook workbook = Workbook.getWorkbook(new File(nomeArquivo));
+            //Pega apenas a primeira aba da planilha
+            Sheet aba = workbook.getSheet(0);
+            Cell celulaTabela;
+            Cell celula;
+            for(int i = 0; i<matrizGrafo.length; i++){
+                celula = aba.getCell(i+1, 0);
+                bairros[i] = celula.getContents();
+                for(int j = 0; j< matrizGrafo.length;j++){
+                    celulaTabela = aba.getCell(i+1, j+1);
+                    matrizGrafo[i][j] = celulaTabela.getContents();
+                } 
+            }
+            workbook.close();
+            
+            grafo.setBairros(bairros);
+            grafo.organizaMatrizes(matrizGrafo);
+            //grafo.imprimeGrafo();
+        } catch (BiffException ex) {
+            
+        }
+        
+    }
+    public boolean isAresta(int origem,int destino){
+        return matrizGrafo[origem][destino]!=null;
+    }
+     public String getBairros(int idBairro){
+        return bairros[idBairro];
+    }
+    
+    public int[] retonaMenorCaminho(int origem, int destino){
+        return grafo.dijkstra(origem, destino); 
+    }
+    public void alteraTempo(int origem,int destino, double novoValor){
+            if(matrizGrafo[origem][destino]!= null){
+                grafo.alteraTempo(origem, destino, novoValor);
+            }
+            else{
+                //Nao sao adjacentes
+            }
+    }
+    
+    
+   /* public void alterarTempo(int origem, int destino, double tempo){
+        ((Aresta)matrizGrafo[origem][destino]).setPesoTempo(tempo);
+        ((Aresta)matrizGrafo[destino][origem]).setPesoTempo(tempo);
+    }*/
+    
+    public void setPreco(double preco){
+        this.precoQuilometro = preco;
+    }
+    
+   /* public double calcularPreco(int origem, int destino){
+        return ((Aresta)matrizGrafo[origem][destino]).getPesoDistancia()*precoQuilometro;
+    }*/
+}
+
+
+
+/*package Controller;
+
 import Util.Fila;
 import Util.Grafo;
 import Util.Lista;
@@ -26,6 +133,13 @@ public class Controller {
        matrizGrafo = new Object[50][50];
        bairros = new String[50];
     }
+    public String getBairros(int i){
+        return bairros[i];
+    }
+
+    public void setBairros(String[] bairros) {
+        this.bairros = bairros;
+    }
     
     public boolean isAresta(int x, int y){
         return matrizGrafo[x][y]!=null;
@@ -44,7 +158,7 @@ public class Controller {
         return total;
     }
    
-    public void insereNaMatriz(String nomeArquivo/*, String[] bairros*/) throws IOException{
+    public void insereNaMatriz(String nomeArquivo/*, String[] bairros*) throws IOException{
         
         try {
             //Pega planilha com todas as abas
@@ -135,4 +249,4 @@ public class Controller {
         }
        // grafo.imprimeGrafo();
     }
-}
+}*/
--- Grafo.java
+++ Grafo.java
@@ -1,5 +1,157 @@
 package Util;
 
+public class Grafo {
+    private String[] bairros;
+    private double[][] matrizDistancias;
+    private double[][] matrizTempo;
+    //private Lista vertices;
+    
+    public Grafo(){
+        this.bairros=new String[50];
+        matrizDistancias = new double[50][50];
+        matrizTempo = new double[50][50];
+    }
+    
+    public int[] dijkstra(int origem, int destino){
+        int[] anteriores = new int[50];
+        int[] visitados = new int[50];
+        double[] tempo = new double[500];
+        int[] caminho = new int[50];
+        
+        for(int i=0;i<500;i++){
+            tempo[i]=Double.MAX_VALUE;//inicializa as variaveis
+        }
+        anteriores[origem]=origem;
+        tempo[origem]=0.0;
+        
+        int atual = origem;
+        int i = 0;
+        while(atual!=destino){
+            visitados[i]=atual;
+
+            int[] adjacentes = retornaAdjacentes(atual);
+            for(int adj:adjacentes){
+                if(!contains(visitados, adj)){
+                    double soma =tempo[atual]+matrizTempo[atual][adj];
+                    if(soma<tempo[adj]){
+                        tempo[adj] = soma;
+                        anteriores[adj]=atual;
+                    }
+                }
+            }
+
+            double menor = Double.MAX_VALUE;
+            for(int adj:adjacentes){
+                if(!contains(visitados, adj) && matrizTempo[atual][adj]<menor){
+                    menor = matrizTempo[atual][adj];
+                    atual = adj;
+                }
+            }
+
+            i++;
+        }
+        
+        double tempoTotal = tempo[destino];
+        i=0;
+        while(true){
+            caminho[i] = atual;
+            if(atual==origem){
+                break;
+            }            
+            atual = anteriores[atual];
+            i++;
+        }
+        
+        return caminho;
+    }
+        
+    public int[] retornaAdjacentes(int origem){//Quem chamar esse metodo tem que tratar se algum adjacente é igual ao destino
+        Fila listaAdjacentes = new Fila();
+        for(int i=0;i<50;i++){
+            if(matrizTempo[origem][i]!= 0.0){
+                listaAdjacentes.inserirFinal(i);
+            }
+        }
+        int[] indicesAdjacentes = new int[listaAdjacentes.obterTamanho()];
+        for(int i=0;i<indicesAdjacentes.length;i++){
+            indicesAdjacentes[i] = (int) listaAdjacentes.removerInicio();       
+        }
+        
+        return indicesAdjacentes;
+    }
+    public void setBairros(String[] bairros) {
+        this.bairros = bairros;
+    }
+    
+    public String getBairros(int idBairro){
+        return bairros[idBairro];
+    }
+    
+    public void organizaMatrizes(Object[][] matrizGrafo){
+            for(int i=0; i<50;i++){
+                for(int j = 0; j<50;j++){
+                        String temp = (String) matrizGrafo[i][j];
+                        String[] separa = temp.trim().split(",");
+                        if(separa.length<=1){
+                            matrizDistancias[i][j]=0.0;
+                            matrizTempo[i][j]=0.0;
+                        }
+                        else{
+                            String distancia = separa[0];
+                            String tempo = separa[1]; 
+                            //Essa parte ta bugando n sei pq, o parseDouble é pra transformar em double e a substring é pra pegar só o q ta depois do =
+                            matrizDistancias[i][j] = Double.parseDouble(distancia.substring(distancia.indexOf("=") + 1, distancia.length()));
+                            matrizTempo[i][j] = Double.parseDouble(tempo.substring(tempo.indexOf("=") + 1, tempo.length())); 
+                        }
+                } 
+            }
+    }
+    
+    //n sei se esse metodo fica aqui ou em aresta  
+    public void alteraTempo(int origem,int destino, double valor){
+        matrizTempo[origem][destino] = valor;
+        matrizTempo[destino][origem] = valor;
+    }
+    
+    public boolean isAresta(int origem,int destino){
+        return matrizDistancias[origem][destino]!=0.0 && matrizTempo[origem][destino]!=0.0;
+    }
+
+    public double getDistancia(int origem, int destino) {
+        return matrizDistancias[origem][destino];
+    }
+    public double getTempo(int origem, int destino){
+        return matrizTempo[origem][destino];
+    }
+    //so pra testar
+    public  void imprimeGrafo(){
+        for (int i = 0; i < matrizDistancias.length; i++) {
+
+            for (int j = 0; j < matrizDistancias[0].length; j++) {
+
+                System.out.print(matrizDistancias[i][j] + "\n");
+            }
+        }
+        
+        for(int i=0;i<50;i++){
+            System.out.println(bairros[i]);
+        }
+    }
+    
+    private boolean contains(int[] array, Object obj){
+        
+        for(Object o:array){
+            if(o.equals(obj)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
+
+/*package Util;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -101,7 +253,7 @@ public class Grafo {
                     
                 }
             }
-        }*/
+        }
     
         public boolean isAresta(int origem,int destino){
             if(matrizDistancias[origem][destino]!=0 && matrizTempo[origem][destino]!=0){
@@ -138,6 +290,6 @@ public class Grafo {
         
         public void setPesoTempo(int tempo){
             pesoTempo = tempo;
-        }*/
+        }
     }
-}
+}*/
